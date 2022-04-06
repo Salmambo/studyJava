489e3472831f6169eedf67fdff6879ea0a7d8d7d
Salmambo <zxcvsqwe@gmail.com>
2022-04-06 20:09:35 +0900

    Update S03_sum.java

--- src/study/S03_sum.java
+++ src/study/S03_sum.java
@@ -9,16 +9,16 @@ public class S03_sum {
 		int max = 0, sum = 0;
 		double avg = 0.0;
 
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				sum += array[i][j];
-				if (array[i][j] > max) {
+		for (int i = 0; i < array.length; i++) { // array의 행 추출
+			for (int j = 0; j < array[i].length; j++) { // 각 행의 값 추출
+				sum += array[i][j]; // 전체 항목의 합
+				if (array[i][j] > max) { // 최대값
 					max = array[i][j];
 				}
 			}
-			avg += array[i].length;
+			avg += array[i].length; // 평균의 분모 자리에 들어갈, 배열 속 개수
 		}
-		avg = sum / avg;
+		avg = sum / avg; // 평균값
 
 		System.out.println("max: " + max);
 		System.out.println("sum: " + sum);
