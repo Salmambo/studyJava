337f20e0a3e95245eb7e08c1fa389d6fc9260084
Salmambo <zxcvsqwe@gmail.com>
2022-04-07 21:49:15 +0900

    Update S04_snail.java

--- src/study/S04_snail.java
+++ src/study/S04_snail.java
@@ -36,7 +36,7 @@ public class S04_snail {
 		for (int i = 1; i < n; i += 2) { // 짝수 번째 행들을 추출
 			for (int j = 0; j < m - 1; j++) { // 그 행들의 열을 추출
 				for (int k = j + 1; k < m; k++) { // 그의 오른쪽 열을 추출
-					if (snail[i][j] < snail[i][k]) { // 짝수 번째 행 안에서 오름차순으로 정렬
+					if (snail[i][j] < snail[i][k]) { // 짝수 번째 행 안에서 내림차순으로 정렬
 						tmp = snail[i][j];
 						snail[i][j] = snail[i][k];
 						snail[i][k] = tmp;
