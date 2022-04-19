0f8d6114d4b639defea013dfd838fe4281c06e57
Salmambo <zxcvsqwe@gmail.com>
2022-04-20 01:25:51 +0900

    Update S07_bankProjcet_example.java

--- src/study/S07_bankProjcet_example.java
+++ src/study/S07_bankProjcet_example.java
@@ -9,6 +9,7 @@ public class S07_bankProjcet_example {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		boolean run = true;
+		List<Object> accounts = new ArrayList<Object>();
 
 //		while (run) {
 //			double balance = Math.round((Math.random() * 10000 + 500) / 1000) * 1000;
@@ -41,17 +42,35 @@ public class S07_bankProjcet_example {
 //		String num = scanner.nextLine().replaceAll(" |-", "");
 //		num = num.substring(0, 3) + "-" +num.substring(3, 6) + "-" + num.substring(6);
 //		System.out.println(num);
-		
-		List<Object> accounts = new ArrayList<Object>();
-		System.out.print("개설을 희망하시는 고객님의 성함을 입력해주십시오. >> ");
-		String owner = scanner.nextLine().replace(" ", "");
-		System.out.print("계좌 비밀번호로 사용할 6자리 숫자를 입력해주십시오. >> ");
-		String tmp = scanner.nextLine();
-		int setPassword = Integer.parseInt(tmp);
-		accounts.add(owner);
-		accounts.add(setPassword);
-		for(Object account : accounts) {
-			System.out.println(account);
+
+//		System.out.print("개설을 희망하시는 고객님의 성함을 입력해주십시오. >> ");
+//		String owner = scanner.nextLine().replace(" ", "");
+//		System.out.print("계좌 비밀번호로 사용할 6자리 숫자를 입력해주십시오. >> ");
+//		String tmp = scanner.nextLine();
+//		int setPassword = Integer.parseInt(tmp);
+//		accounts.add(owner);
+//		accounts.add(setPassword);
+//		for(Object account : accounts) {
+//			System.out.println(account);
+//		}
+//		System.out.println(accounts);
+
+		System.out.println(accounts.size());
+		List<Object> account = new ArrayList<Object>(3);
+		System.out.println(account.size());
+		accounts.add("오브젝트");
+		account.add(1);
+		System.out.println(accounts.size());
+		System.out.println(account.size());
+		account.add(2);
+		account.add(3);
+		account.add(4);
+		System.out.println(account.size());
+		System.out.println(account);
+		int count = 0;
+		while (accounts.size() < 10) {
+			count++;
+			accounts.add(count);
 		}
 		System.out.println(accounts);
 
